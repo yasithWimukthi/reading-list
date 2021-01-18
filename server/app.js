5cb563864f2ea6d5245fdf8d93e9ed55880ca2c6
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-18 23:38:24 +0530

    create graphQL schema

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
+// noinspection JSValidateTypes
 app.use('/graphql',graphqlHTTP({
-
+    schema,
+    graphiql : true
 }))
 
 app.listen(4000,()=>{
